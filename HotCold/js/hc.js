bdb423a41607eec17c78f1115824c1bf9bf65eea
thespareroomstudio <Jason@Jasons-MBA.local>
2013-11-18 21:58:11 +0300

    Worked on background changing color.
    Did a lot of back and forth and tried many approaches.  Not many things
    worked out as planned.  Have a working background color change, added
    instructions.  Noticed a new bug.  Will work on that for the next
    version.

--- HotCold/js/hc.js
+++ HotCold/js/hc.js
@@ -1,9 +1,12 @@
 $(function(){
 	var secretNum = init();
+	var userHistory = null;
+
 	console.log("The secret number is: " + secretNum);
 	$('td').click(function() {
 		var userNum = $(this).text();
 		console.log("User choose: " + userNum);
+		
 		var feedback = compare(userNum, secretNum);
 		$('#message').text(feedback);
 		$(this).css("background-color", "inherit");
@@ -11,28 +14,26 @@ $(function(){
 	
 
 	function init() {
-		var secretNum = Math.floor((Math.random()*50)+1);  // pick a random integer
+		var secretNum = Math.floor((Math.random()*100)+1);  // pick a random integer
 		var radialX = secretNum.toString();
 		var radialY = secretNum.toString();
 
 		radialX = radialX.substr(1,1);
-		console.log(radialX);
-		
 		radialY = radialY.substr(0,1);
-		console.log(radialY);
+		console.log("First digit is: "+radialY);
+		console.log("Second digit is: "+radialX);
 		
 		if (radialX == "0") {
 			radialX = 10;
 		} else
 		if (radialX == "") {
 			radialX = radialY;
-			radialY == 1;
+			radialY == 0;
 		}
-		console.log(radialX, radialY);
+		console.log("x is: "+radialX, " | y is: "+radialY);
 		radialX = radialX * 50 + 25;																				
 		radialY = radialY * 50 + 25;
 		console.log(radialX, radialY);																				
-		
 		$('table').css("background", "radial-gradient(circle at "+radialX+"px "+radialY+"px, red, orange, yellow, #F0F8FF, #6495ED, #00008B)");
 		
 		return secretNum;
@@ -40,10 +41,14 @@ $(function(){
 
 	function compare(userNum, secretNum) {
 		var difference = (userNum - secretNum);
-		if (difference < 0) {
-			difference = difference * -1;
+
+		difference = absolute(difference);
+		
+		if (userHistory) {
+			setBackgroundColor(difference);
 		}
-		console.log("The difference between them is: " + difference);
+		userHistory = difference;
+		
 		if (userNum == secretNum) {
 			return "Ding ding ding!  You got it!";
 		} else
@@ -72,18 +77,41 @@ $(function(){
 			return "Somebody turn off the A//C, I'm freezing!";
 		} else
 		if (difference > 75 && difference <= 85) {
-			return "I can see my breath!  I should've brought mittens";
+			return "I can see my breath!  I should've brought mittens.";
 		} else
 		if (difference > 85 & difference <= 90) {
 			return "It's so cold it's snowing.  Who wants to make snow angels!?";
 		} else
 		if (difference > 90 && difference <= 95) {
-			return "I can't feel my toes, it's so cold.  I want to go home";
+			return "I can't feel my toes, it's so cold.  I want to go home.";
 		} else
 		if (difference > 95) {
-			return "Welome to the Ice Age.  Seriously, you can't get any colder than this";
+			return "Welome to the Ice Age.  Seriously, you can't get any colder than this.";
 		} else
 		return "There was an error.";
-	}
+	};
+
+	function setBackgroundColor(difference) {
+		var warm = "#EA0000";
+		var cold = "blue";
+		if (difference > userHistory) {
+			$('body').animate( { backgroundColor: "blue" }, 500 );
+		} else {
+			$('body').animate( { backgroundColor: warm}, 500 );
+		};
+	};
+
+	function absolute(x) {
+		console.log("x is: " + x);
+		if (x < 0) { 
+			x = x * 1
+		}
+		return x;
+	};
+	$('.target').blurjs({
+	source: 'body',
+	overlay: 'rgba(255,255,255,0.4)'
+
+});
 
 });
